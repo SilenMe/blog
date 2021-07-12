e14c5fe17cce3b28c5610fd23ff7fe2bdb56b8f5
SilenMe <vivek2vks@gmailcom>
2021-07-12 16:04:33 +0530

    blueprinting done

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os #for env var
+
+
+class Config:
+    SECRET_KEY = '3090657a7c7bbbfebf02f365802085fe'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
---
+++ flaskblog/posts/__init__.py
@@ -0,0 +1,3 @@
+#this file help in telling jango that it's a package folder 
+
+#note that it also may be empty
